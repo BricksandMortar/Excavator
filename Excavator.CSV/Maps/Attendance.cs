6c0da2dfb38a31b24120a669d909fdafc8aa80cb
Arran France <arranf@gmail.com>
2017-04-15 16:53:35 +0100

    Remove unneeded imports

--- Excavator.CSV/Maps/Attendance.cs
+++ Excavator.CSV/Maps/Attendance.cs
@@ -1,21 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Globalization;
 using System.Linq;
-using Excavator.Utility;
-using Ical.Net;
-using Ical.Net.DataTypes;
-using Ical.Net.Interfaces.DataTypes;
-using Ical.Net.Serialization;
-using Ical.Net.Serialization.iCalendar.Serializers;
-using NodaTime;
 using NodaTime.Text;
 using Rock;
 using Rock.Data;
 using Rock.Model;
-using Rock.Web.Cache;
-using Attribute = Rock.Model.Attribute;
 
 namespace Excavator.CSV
 {
